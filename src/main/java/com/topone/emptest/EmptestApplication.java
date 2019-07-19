50a91aeb0bd8e9df01ca340d4ff83e62123d7529
1620591089 <1620591089@qq.com>
2019-07-19 11:56:37 +0800

    添加cs

---
+++ src/main/java/com/topone/emptest/EmptestApplication.java
@@ -0,0 +1,13 @@
+package com.topone.emptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmptestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/topone/emptest/domain/Emp.java
@@ -0,0 +1,75 @@
+package com.topone.emptest.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Emp implements Serializable {
+    private Integer empno,mgr,deptno;
+    private String ename,job;
+    private double sal,comm;
+    private Date hireDate;
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public double getSal() {
+        return sal;
+    }
+
+    public void setSal(double sal) {
+        this.sal = sal;
+    }
+
+    public double getComm() {
+        return comm;
+    }
+
+    public void setComm(double comm) {
+        this.comm = comm;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+}
